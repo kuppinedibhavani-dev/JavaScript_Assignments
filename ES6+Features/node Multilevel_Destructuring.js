2742b32b186f25850dc3dd50044cee4c86e51d0c
kuppinedibhavani_dev <kuppinedibhavani@gmail.com>
2025-11-22 16:55:47 +0530

    Multi level Destructuring

---
+++ ES6+Features/node Multilevel_Destructuring.js
@@ -0,0 +1,16 @@
+const people = [ {
+     name: "Alice",
+      address:{
+       city: "New York", 
+       street: { name: "Broadway", number: 123 } } },
+     { 
+        name: "Bob",
+         address:{
+             city: "Los Angeles",
+              street: { name: "Sunset Boulevard", number: 456 } } }
+             ];
+             const formatPeople=(people)=>{
+    return people.map(({name,address:{city,street:{name:streetName}}
+            })=>`${name} lives in ${city} on ${streetName}`);
+        }
+     console.log(formatPeople(people));
